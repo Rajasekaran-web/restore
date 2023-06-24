57cd734e11e7b24707588fab1c68a38b70cfe094
RAJASEKARAN <116161691+Rajasekaran-web@users.noreply.github.com>
2023-06-24 14:10:03 +0530

    Add files via upload

---
+++ project/restore.js
@@ -0,0 +1,7 @@
+let slider=document.querySelector(".splider-images");
+let sliderMain=document.querySelector(".splider-main")
+let image=document.querySelectorAll(".img")
+
+let index=1;
+
+let firstClone=.img
